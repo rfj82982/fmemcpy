033e567b37cbe59695b0f72cea0714a16f12b82a
Paul <ptb0890@gmail.com>
2023-12-18 21:22:53 +0000

    Add check that source and destination both hold > n bytes

--- src/fmemcpy.f90
+++ src/fmemcpy.f90
@@ -7,6 +7,8 @@
 module fmemcpy
 
   use iso_c_binding
+
+  use utils
   
   implicit none
 
@@ -25,16 +27,50 @@ module fmemcpy
   
 contains
 
+  ! Safe interface to memcpy, checks that the size of the source and destination arrays is large
+  ! enough to transfer n bytes.
+  !
+  ! Note that the destination is inout, only the n bytes specified will be overwritten.
   subroutine memcpy(dst, src, n)
 
-    type(*), dimension(..), target :: dst             ! Destination buffer
-    type(*), dimension(..), target, intent(in) :: src ! Source buffer
+    class(*), dimension(..), intent(inout) :: dst ! Destination buffer
+    class(*), dimension(..), intent(in) :: src    ! Source buffer
     integer, intent(in) :: n ! Number of bytes to copy
 
-    ! Do some safety checking?
-    
-    call cmemcpy(c_loc(dst), c_loc(src), int(n, c_size_t))
+    call check_buffers(dst, src, n)
+    call memcpy_c(dst, src, n)
     
   end subroutine memcpy
+
+  ! Internal interface to memcpy - everything is treated as TYPE(*)/void* so no size checking can be
+  ! performed.
+  subroutine memcpy_c(dst, src, n)
+
+    type(*), dimension(..), target, intent(inout) :: dst ! Destination buffer
+    type(*), dimension(..), target, intent(in) :: src    ! Source buffer
+    integer, intent(in) :: n ! Number of bytes to copy
+
+    call cmemcpy(c_loc(dst), c_loc(src), int(n, c_size_t))
+    
+  end subroutine memcpy_c
+
+  ! Utility subroutine to check source and destination buffers are large enough to accomodate n
+  ! bytes.
+  subroutine check_buffers(dst, src, n)
+
+    class(*), dimension(..), intent(inout) :: dst ! Destination buffer
+    class(*), dimension(..), intent(in) :: src    ! Source buffer
+    integer, intent(in) :: n ! Number of bytes to copy
+
+    if (.not. is_buffer_safe(dst, n)) then
+       print *, "Destination array is too small to store ", n, " bytes"
+       stop 1
+    end if
+    if (.not. is_buffer_safe(src, n)) then
+       print *, "Source array is too small to store ", n, " bytes"
+       stop 1
+    end if
+    
+  end subroutine check_buffers
   
 end module fmemcpy
